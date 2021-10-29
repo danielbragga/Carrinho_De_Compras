fe46087038b325aae79e3947b027df1ab0446fa6
danielbragga <89660062+danielbragga@users.noreply.github.com>
2021-10-29 12:09:42 -0300

    Delete test/app/modules directory

--- test/app/modules/loja/loja_page_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-main() {
-  testWidgets('LojaPage has title', (WidgetTester tester) async {
-    //  await tester.pumpWidget(buildTestableWidget(LojaPage(title: 'Loja')));
-    //  final titleFinder = find.text('Loja');
-    //  expect(titleFinder, findsOneWidget);
-  });
-}
