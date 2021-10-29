8521fb574ec4fe5471b4550a3411da7ceac47104
danielbragga <89660062+danielbragga@users.noreply.github.com>
2021-10-29 12:10:59 -0300

    Add files via upload

---
+++ lib/app/modules/pagamento/pages/pagamento_com_sucesso/pagamento_sucesso_controller.g.dart
@@ -0,0 +1,24 @@
+part of 'pagamento_sucesso_controller.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
+
+mixin _$PagamentoSucessoController on _PagamentoSucessoControllerBase, Store {
+  Computed<PagamentoModel> _$modelComputed;
+
+  @override
+  PagamentoModel get model =>
+      (_$modelComputed ??= Computed<PagamentoModel>(() => super.model,
+              name: '_PagamentoSucessoControllerBase.model'))
+          .value;
+
+  @override
+  String toString() {
+    return '''
+model: ${model}
+    ''';
+  }
+}
